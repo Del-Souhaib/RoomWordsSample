2207506d80f715853a6dc296e8d380b74da126b7
souhaib <del.souhaub@gmail.com>
2022-05-17 00:45:48 +0100

    Initial commit

---
+++ app/src/main/java/ma/projet/roomwordssample/recyclerview_item.java
@@ -0,0 +1,14 @@
+package ma.projet.roomwordssample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class recyclerview_item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recyclerview_item);
+    }
+}
